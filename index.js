9bcaa22fc104a159e1dc10a361c31dcf4a53b081
Iqoqacola <iqoqacola@gmail.com>
2024-05-25 20:34:33 +0800

    add method delete cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cors = require('cors')
 const app = express();
 
 //Middleware
-app.use(cors({ origin: true, credentials: true, methods: 'GET,PUT,POST,OPTIONS', allowedHeaders: 'Content-Type,Authorization' }));
+app.use(cors({ origin: true, credentials: true, methods: 'GET,PUT,POST,DELETE', allowedHeaders: 'Content-Type,Authorization' }));
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, ": ", req.method);
